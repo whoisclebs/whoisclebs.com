7d1eaa4502685db555fcd6e15c80569310c2769c
whoisclebs <43012757+whoisclebs@users.noreply.github.com>
2025-03-09 18:14:49 -0300

    chore: update footer copyright information

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="py-6 border-t border-gray-100">
       <div className="blog-container text-center text-sm text-gray-500">
-        © {new Date().getFullYear()} WhoIsClebs. Feito com ❤️ e Vite.
+        © 2022 - {new Date().getFullYear()} Clebson A. Fonseca. Feito com ❤️ e Vite.
       </div>
     </footer>
   );
